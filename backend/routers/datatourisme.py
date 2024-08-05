42cb744005709e2581d94699dbfa2c45e2309c1c
Olivier Douangvichith <olivier.douangvichith@gmail.com>
2024-08-05 15:25:43 +0200

    Split programs into FastAPI

--- backend/routers/datatourisme.py
+++ backend/routers/datatourisme.py
@@ -9,8 +9,6 @@ logger.setLevel(logging.DEBUG)
 routerDataTourisme = APIRouter()
 
 
-
-
 @routerDataTourisme.get("/", response_description="Data Tourisme")
 async def hello():
     return {"data": "Hello Data Tourisme"}
@@ -19,9 +17,4 @@ async def hello():
 @routerDataTourisme.post("/graph", response_description="Data Tourisme")
 async def create_graph(data: dict = Body(...)):
     # await asyncio.sleep(30)
-    return {
-        "latitude": data.latitude,
-        "longitude": data.longitude,
-        "poi_types": data.poi_types,
-        "radius": data.radius
-    }
+    return {"received_data": data}
